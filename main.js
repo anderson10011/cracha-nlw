144ecb26fc5f089a61cdcee4143b991ad747c9ce
anderson10011 <76177969+anderson10011@users.noreply.github.com>
2021-10-21 23:39:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+  github: 'anderson10011',
+  youtube: 'jakelinygracielly',
+  facebook: 'maykbrito',
+  instagram: 'jakeliny.gracielly',
+  twitter: 'jakelinytec'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(Response => Response.json())
+    .then(data => {
+      userName.textContent = data.login
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserPhoto.src = data.avatar_url
+      UserLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
